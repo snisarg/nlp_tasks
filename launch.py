3d64b277d4ae004bd842d09d2fbc7c2f96e17092
Nisarg Shah <snisarg@gmail.com>
2020-12-09 13:21:21 -0800

    Setting up classes to work with Hydra

---
+++ launch.py
@@ -0,0 +1,13 @@
+import hydra
+from omegaconf import OmegaConf
+
+from nlp_tasks import Config
+
+
+@hydra.main(config_path="conf", config_name="config")
+def my_app(cfg: Config) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    my_app()
