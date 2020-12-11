52a9381f458a1c32f3ee2aece1db21786ff04c27
Nisarg Shah <snisarg@gmail.com>
2020-12-10 19:43:02 -0800

    Registering configen generated dataclass, hierarchy

--- nlp_tasks/__init__.py
+++ nlp_tasks/__init__.py
@@ -4,21 +4,11 @@ from dataclasses import dataclass
 
 from hydra.core.config_store import ConfigStore
 from hydra_text.text.decoder.mlp import ActivationFnConf, MLPDecoderConf, GeLUConf
+from hydra_text.text.models.manual_additions import ModelConf
+from hydra_text.text.models.transformer import RobertaModelConf
 from hydra_text.text.representation.transformers import TransformerLayerConf
 from omegaconf import MISSING
 from text.decoder.mlp import GeLU
-
-
-@dataclass
-class ModelConf:
-    pass
-
-
-@dataclass
-class TransformerModelConf(ModelConf):
-    activation_fn: Any = GeLUConf()
-    transformer: TransformerLayerConf = TransformerLayerConf(attention=None, residual_mlp=None)
-    mlp: MLPDecoderConf = MLPDecoderConf(in_dim=10, out_dim=2, bias=False, hidden_dims=[1], activation=GeLU)
 
 
 @dataclass
@@ -39,7 +29,6 @@ class Config:
 
 cs = ConfigStore.instance()
 cs.store(group="task", name="task", node=TaskConf)
-cs.store(group="task/model", name="transformer_model", node=TransformerModelConf)
-cs.store(group="task/model/activation_fn", name="gelu", node=GeLUConf)
+cs.store(group="task/model", name="roberta_model", node=RobertaModelConf)
 cs.store(group="trainer", name="trainer", node=TrainerConf)
 cs.store(name="config", node=Config)
--- nlp_tasks/task/doc_classification.py
+++ nlp_tasks/task/doc_classification.py
@@ -1,5 +1,9 @@
+import hydra
 import pytorch_lightning as pl
 
+from nlp_tasks import ModelConf
+
+
 class DummyClassificationTask(pl.LightningModule):
-    def __init__(self):
-        pass
+    def __init__(self, model: ModelConf):
+        self.model = hydra.utils.instantiate(model)
