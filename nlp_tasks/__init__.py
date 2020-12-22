373137c3f9c6221bdd5930cd088e7f5e9718aadb
Nisarg Shah <snisarg@gmail.com>
2020-12-21 22:23:22 -0800

    Fix warnings

--- nlp_tasks/__init__.py
+++ nlp_tasks/__init__.py
@@ -3,20 +3,19 @@ from typing import Any
 from dataclasses import dataclass
 
 from hydra.core.config_store import ConfigStore
-from hydra_text.text.decoder.mlp import ActivationFnConf, MLPDecoderConf, GeLUConf
 from hydra_text.text.models.manual_additions import ModelConf
 from hydra_text.text.models.transformer import RobertaModelConf
-from hydra_text.text.representation.transformers import TransformerLayerConf
 from omegaconf import MISSING
-from text.decoder.mlp import GeLU
 
 from nlp_tasks.model.transformers import DocModelConf
 
 
 @dataclass
 class TaskConf:
+    data: Any = MISSING
     model: ModelConf = MISSING
     optim: Any = MISSING
+    transforms: Any = MISSING
 
 @dataclass
 class TrainerConf:
@@ -25,8 +24,8 @@ class TrainerConf:
 
 @dataclass
 class Config:
-    task: TaskConf
-    trainer: TrainerConf
+    task: TaskConf = MISSING
+    trainer: TrainerConf = MISSING
 
 
 cs = ConfigStore.instance()
